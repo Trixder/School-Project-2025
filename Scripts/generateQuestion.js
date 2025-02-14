8c50e5e73a46cd7075beca1367b99557f0c514ba
Trixder <119891580+Trixder@users.noreply.github.com>
2025-02-14 13:05:31 +0100

    update

---
+++ Scripts/generateQuestion.js
@@ -0,0 +1,8 @@
+function GenerateQuestions() {
+    const folderPath = "../HTML/Templates"
+    const fileName = "Testing.json"
+
+    //foeach question in questions
+    //switch (type)
+    //create question slide
+}
